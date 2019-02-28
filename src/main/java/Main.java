aba00e3f9252efb2e9a9e3a985080ca5e6316aec
Hamza Cherif Yacine <yacine.hamzacherif@gmail.com>
2019-02-28 19:27:51 +0100

    read file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,29 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
+        Main main = new Main();
+        main.readFile();
+    }
 
+
+    public void readFile() {
+
+        try {
+            AtomicInteger counter = new AtomicInteger(0);
+            Set photos = Files.lines(Paths.get(getClass().getResource("a_example.txt").getPath()))
+                    .skip(1)
+                    .map(s -> s.split("\\s"))
+                    .map(photo -> new Photo(photo[0].equals("V"), Stream.of(photo).skip(2).collect(Collectors.toSet())))
+                    .collect(Collectors.toSet());
+        photos.forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/Photo.java
@@ -0,0 +1,23 @@
+import java.util.Set;
+
+public class Photo {
+    static int n = 0;
+    public Set<String> tags;
+    public boolean isVertical;
+    public int id;
+
+    public Photo(boolean isVertical, Set<String> tags) {
+        this.tags = tags;
+        this.isVertical = isVertical;
+        this.id = n++;
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "tags=" + tags +
+                ", isVertical=" + isVertical +
+                ", id=" + id +
+                '}';
+    }
+}
