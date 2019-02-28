e5334c1b8e29ab0af3a6b3376dd1e8853f7d1685
Hamza Cherif Yacine <yacine.hamzacherif@gmail.com>
2019-02-28 19:30:32 +0100

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,26 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
-        main.readFile();
+        main.readFile("a_example.txt");
     }
 
 
-    public void readFile() {
-
+    public Set<Photo> readFile(String file) {
         try {
-            AtomicInteger counter = new AtomicInteger(0);
-            Set photos = Files.lines(Paths.get(getClass().getResource("a_example.txt").getPath()))
+            return Files.lines(Paths.get(getClass().getResource(file).getPath()))
                     .skip(1)
                     .map(s -> s.split("\\s"))
                     .map(photo -> new Photo(photo[0].equals("V"), Stream.of(photo).skip(2).collect(Collectors.toSet())))
                     .collect(Collectors.toSet());
-        photos.forEach(System.out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
     }
 }
