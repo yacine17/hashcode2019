820bc8d5de02420db656d045775b639a051a0ece
Hamza Cherif Yacine <yacine.hamzacherif@gmail.com>
2019-02-28 19:33:32 +0100

    refactor2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,12 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Main main = new Main();
-        main.readFile("a_example.txt");
+        readFile("a_example.txt");
     }
 
-
-    public Set<Photo> readFile(String file) {
+    public static Set<Photo> readFile(String file) {
         try {
-            return Files.lines(Paths.get(getClass().getResource(file).getPath()))
+            return Files.lines(Paths.get(Main.class.getResource(file).getPath()))
                     .skip(1)
                     .map(s -> s.split("\\s"))
                     .map(photo -> new Photo(photo[0].equals("V"), Stream.of(photo).skip(2).collect(Collectors.toSet())))
